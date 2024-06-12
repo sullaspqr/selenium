b1c08e7e2b61a36fcd81f371a3d5e6136e234f92
sullaspqr <sulla.spqr78@gmail.com>
2024-06-12 17:25:51 +0200

    xd commit

---
+++ xd.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
+options = webdriver.ChromeOptions()
+driver = webdriver.Chrome(options=options)
+driver.set_window_size(1920, 1080)
+
+driver.get("https://szallas.sulla.hu/")
+driver.find_element(By.XPATH, '//input[@name="email"]').send_keys('user@kodbazis.hu')
+time.sleep(1)
+driver.find_element(By.XPATH, '//input[@name="password"]').send_keys('teszt')
+time.sleep(1)
+#driver.find_element(By.XPATH, '//input[@name="password"]').clear()
+driver.find_element(By.TAG_NAME, 'button').click()
+driver.save_screenshot('screenshot.png')
+time.sleep(1)
+driver.quit()
