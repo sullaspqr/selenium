4f2f2553c31a6ad4d1d8db6f2e716fa57184a0a7
sullaspqr <sulla.spqr78@gmail.com>
2024-06-12 17:22:47 +0200

    screenshot 1 változás

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ driver = webdriver.Chrome(options=options)
 driver.set_window_size(1920, 1080)
 
 driver.get("https://www.python.org")
-driver.save_screenshot('screenshot.png')
+driver.save_screenshot('screenshot1.png')
 time.sleep(3)
 elem = driver.find_element(By.LINK_TEXT, value="Success Stories")
 if not(elem.is_displayed()):
