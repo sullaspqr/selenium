2bfc783b4bf112c4150b228dbbac08681ca7dab8
sullaspqr <sulla.spqr78@gmail.com>
2024-06-12 17:32:39 +0200

    törölve

--- test2.py
+++ test2.py
@@ -9,6 +9,5 @@ driver.set_window_size(1920, 1080)
 driver.get("https://www.python.org/downloads/")
 driver.save_screenshot('screenshot.png')
 elements = driver.find_elements(By.CLASS_NAME, 'release-download')
-time.sleep(1)
 for e in elements:
     print(e.text)
